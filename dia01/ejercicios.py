05a9de5bbf329bd6e12ad1b035456532ef8bd4bb
Ronald Tomas Aguirre Mori <aguirremori@gmail.com>
2026-07-11 13:40:47 -0500

    Día 1 - Variables y primer programa en Python

---
+++ dia01/ejercicios.py
@@ -0,0 +1,15 @@
+nombre = "Carlos Perez"
+cargo = "Planner"
+sueldo = 4500
+bono = 300
+empresa = "Metso"
+
+print("============ EMPLEADO ==============")
+print()
+print("Nombre:", nombre)
+print("Cargo:", cargo)
+print("Sueldo: S/", sueldo)
+print("Bono: S/",bono)
+print("Empresa:", empresa)
+print()
+print("====================================")
