31e9d399bfc07f2f0d308a5eebda60542e48caa9
Ronald Tomas Aguirre Mori <aguirremori@gmail.com>
2026-07-20 17:13:07 -0500

    Día 6 - Bucles while

---
+++ dia06/while01.py
@@ -0,0 +1,20 @@
+# #Ejercicio 1
+# contador = 1
+
+# while contador < 11:
+#     print(contador)
+#     contador+=1
+
+# #Ejercicio 2
+# contador = 10
+
+# while contador > 0:
+#     print(contador)
+#     contador-=1
+
+#Ejercicio 3
+cantidad = 1
+
+while cantidad < 6:
+    print(f"Empleado {cantidad}")
+    cantidad+=1
---
+++ dia06/while02.py
@@ -0,0 +1,5 @@
+contador = 2
+
+while contador < 21:
+    print(contador)
+    contador +=2
