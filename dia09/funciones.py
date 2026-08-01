1e12990d2e60d7c62cf5b651aa5d1cf0d9e50e85
Ronald Tomas Aguirre Mori <aguirremori@gmail.com>
2026-08-01 12:40:59 -0500

    Dia 9 - Funciones

---
+++ dia09/funciones.py
@@ -0,0 +1,34 @@
+#Ejercicio 1
+
+# def mostrar_menu():
+#     print("======MENU======")
+#     print("1.Registrar empleado")
+#     print("2.Buscar empleado")
+#     print("3.Salir")
+#     print("================")
+
+# mostrar_menu()
+
+#Ejercicio 2
+
+# def saludar():
+#     print("Bienvenido al sistema de planillas")
+
+# contador=0
+# while contador <3:
+#     saludar()
+#     contador+=1
+
+#Ejercicio 3
+
+def linea():
+    print("----------------")
+
+linea()
+print("Empleado")
+linea()
+print("Sueldo")
+linea()
+
+
+
