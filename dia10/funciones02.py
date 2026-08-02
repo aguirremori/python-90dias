fcc7999f0f26f0f5f143d8de8c1200370ea6597c
Ronald Tomas Aguirre Mori <aguirremori@gmail.com>
2026-08-02 09:32:31 -0500

    Dia 10 - Funciones con varibles

---
+++ dia10/funciones02.py
@@ -0,0 +1,34 @@
+#Ejercicio 1
+
+# def mostrar_empleado(nombre):
+#     print("Empleado: ",nombre)
+
+# mostrar_empleado("Carlos")
+# mostrar_empleado("Laura")
+# mostrar_empleado("Pedro")
+
+#Ejercicio 2
+
+# def mostrar_sueldo(nombre,sueldo):
+#     print("Empleado: ", nombre)
+#     print("Sueldo: S/ ",sueldo)
+
+# mostrar_sueldo("Carlos",3500)
+# mostrar_sueldo("Tom",3600)
+# mostrar_sueldo("Raquel",4000)
+
+#Ejercicio 3
+
+# def sumar(numero1,numero2):
+#     total = numero1 + numero2
+#     print(total)
+
+# sumar(8,5)
+
+#Ejercicio 4 (Reto)
+
+def calcular_bruto(sueldo,bono):
+    sueldo_bruto = sueldo + bono
+    print("Sueldo bruto: S/",sueldo_bruto)
+
+calcular_bruto(5000,1000)
