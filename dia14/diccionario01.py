de04c4792460c2e9468d671166138d051745ee5b
Ronald Tomas Aguirre Mori <aguirremori@gmail.com>
2026-08-06 20:20:44 -0500

    Dia 14 - Diccionarios

---
+++ dia14/diccionario01.py
@@ -0,0 +1,17 @@
+empleado = {
+    "nombre" : "Tom",
+    "cargo" : "Planning Manager",
+    "edad" : 41
+}
+
+print(empleado["nombre"])
+print(empleado["cargo"])
+print(empleado["edad"])
+
+empleado["cargo"] = "Supply Chain Manager"
+
+print(empleado) 
+
+empleado["empresa"] = "Metso"
+
+print(empleado)
---
+++ dia14/reto.py
@@ -0,0 +1,9 @@
+producto = {
+    "codigo" : "P001",
+    "descripcion" : "Bomba Centrifuga",
+    "precio" : 4500,
+    "stock" : 12
+}
+
+for key in producto :
+    print(f"{key} : {producto[key]}") 
